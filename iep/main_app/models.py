755583b4e4fe3f2717ab03de74e55929c231a2f8
Pratham Rathi <pratham.02rathi@gmail.com>
2023-02-18 00:22:46 -0800

    made models

--- iep/main_app/models.py
+++ iep/main_app/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+import uuid
 
 # Create your models here.
+class Student(models.Model):
+    uuid = models.UUIDField(primary_key=True, unique = True, default = uuid.uuid4(), editable = False)
+    name = models.CharField(max_length=255)
+    grade = models.IntegerField(default = 0)
+
+class LessonPlan(models.Model):
+    uuid = models.UUIDField(primary_key=True, unique = True, default = uuid.uuid4(), editable = False)
+    name = models.CharField(max_length=255)
+    overview = models.TextField()
+    objectives = models.TextField()
+
+class IEP(models.Model):
+    uuid = models.UUIDField(primary_key=True, unique = True, default = uuid.uuid4(), editable = False)
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    accomodation = models.TextField()
+
+class LpsAccomodation(models.Model):
+    uuid = models.UUIDField(primary_key=True, unique = True, default = uuid.uuid4(), editable = False)
+    lesson_plan = models.ForeignKey(LessonPlan, on_delete=models.CASCADE)
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    accomodation = models.TextField()
